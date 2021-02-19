85fbe16f684554ccca522d80672719d541b61e0a
Ahmed Al Salih <ahmed82@users.noreply.github.com>
2021-02-19 03:46:30 -0500

    rename the contract Name for best practice and add the contract solidity file for refrence

---
+++ AhmedContract.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.18;
+//pragma solidity >=0.7.0 <0.8.0;
+
+contract AhmedContract {
+    
+   string fName;
+   uint age;
+   
+   function setInstructor(string _fName, uint _age) public {
+       fName = _fName;
+       age = _age;
+   }
+   
+   function getInstructor() public constant returns (string, uint) {
+       return (fName, age);
+   }
+    
+}
